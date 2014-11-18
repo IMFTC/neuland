22a459c23832882358afa457d8108a2c19973164
Volker Sobek <reklov@live.com>
2014-11-18 14:55:25 +0100

    request create widget: Add "valid-data" property
    Will be used to enable/disable the win.send-request action.

--- src/neuland-request-create-widget.c
+++ src/neuland-request-create-widget.c
@@ -29,9 +29,19 @@ struct _NeulandRequestCreateWidgetPrivate
   GtkEntry *tox_id_entry;
   GtkTextBuffer *text_buffer;
   GtkButton *add_button;
+
+  gboolean valid_data;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (NeulandRequestCreateWidget, neuland_request_create_widget, GTK_TYPE_BOX)
+
+enum {
+  PROP_0,
+  PROP_VALID_DATA,
+  PROP_N
+};
+
+static GParamSpec *properties[PROP_N] = {NULL, };
 
 /* static void
  * on_entry_activate (NeulandRequestCreateWidget *widget,
@@ -83,8 +93,29 @@ on_tox_id_entry_changed (GObject *gobject,
     gtk_style_context_remove_class (context, "error");
   else
     gtk_style_context_add_class (context, "error");
+
+  priv->valid_data = address_is_valid;
+  g_object_notify_by_pspec (gobject, properties[PROP_VALID_DATA]);
 }
 
+static void
+neuland_request_create_widget_get_property (GObject *object,
+                                            guint property_id,
+                                            GValue *value,
+                                            GParamSpec *pspec)
+{
+  NeulandRequestCreateWidget *widget = NEULAND_REQUEST_CREATE_WIDGET (object);
+
+  switch (property_id)
+    {
+    case PROP_VALID_DATA:
+      g_value_set_boolean (value, widget->priv->valid_data);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
 
 static void
 neuland_request_create_widget_class_init (NeulandRequestCreateWidgetClass *klass)
@@ -92,13 +123,25 @@ neuland_request_create_widget_class_init (NeulandRequestCreateWidgetClass *klass
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/org/tox/neuland/neuland-request-create-widget.ui");
+  gobject_class->get_property = neuland_request_create_widget_get_property;
 
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/tox/neuland/neuland-request-create-widget.ui");
   gtk_widget_class_bind_template_child_private (widget_class, NeulandRequestCreateWidget, tox_id_entry);
   gtk_widget_class_bind_template_child_private (widget_class, NeulandRequestCreateWidget, text_buffer);
-
   /* gtk_widget_class_bind_template_callback (widget_class, on_entry_activate); */
   gtk_widget_class_bind_template_callback (widget_class, on_tox_id_entry_changed);
+
+  properties[PROP_VALID_DATA] =
+    g_param_spec_boolean ("valid-data",
+                          "Valid data",
+                          "TRUE if the given Tox ID is valid",
+                          FALSE,
+                          G_PARAM_READABLE |
+                          G_PARAM_CONSTRUCT);
+
+  g_object_class_install_properties (gobject_class,
+                                     PROP_N,
+                                     properties);
 }
 
 /* String owned by the widget! */
